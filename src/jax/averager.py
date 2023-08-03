1e04a284ad09449d823d380b0641d0e3e263b829
maorivgi <maorivgi@mail.huji.ac.il>
2023-08-03 13:23:32 +0300

    optimized norm computation to avoid for loops

--- src/jax/averager.py
+++ src/jax/averager.py
@@ -1,24 +1,23 @@
-from typing import Optional, NamedTuple
-
 import chex
-from optax._src import utils
-import optax
 import jax
 import jax.numpy as jnp
-from optax._src import base, combine, transform
+import optax
+from optax._src import base
+from optax._src import utils
+from typing import NamedTuple
+
 
 class PolynomialAveragingState(NamedTuple):
     """State for the Polynomial decay averaging algorithm."""
     count: chex.Array  # shape=(), dtype=jnp.int32.
     av_model: base.Params
 
+
 # the implementation is based on the transform.ema
 def polynomial_decay_averaging(
         gamma: float = 8,
 ) -> base.GradientTransformation:
-
     accumulator_dtype = utils.canonicalize_dtype(None)
-    
 
     def init_fn(params):
         return PolynomialAveragingState(
@@ -27,13 +26,13 @@ def polynomial_decay_averaging(
                 lambda t: jnp.zeros_like(t, dtype=accumulator_dtype), params))
 
     def update_fn(updates, state, params=None):
-        count_inc = optax.safe_int32_increment(state.count)  # should be used in dog as well
+        count_inc = optax.safe_int32_increment(state.count)
 
         t = state.count
         av_coef = (1 - ((gamma + 1) / (gamma + t)))
         old_coef = ((gamma + 1) / (gamma + t))
         new_av = jax.tree_util.tree_map(
-            lambda av, p, u: av_coef * av + old_coef * (p+u), state.av_model, params, updates)
+            lambda av, p, u: av_coef * av + old_coef * (p + u), state.av_model, params, updates)
 
         av_model = utils.cast_tree(new_av, accumulator_dtype)
         return updates, PolynomialAveragingState(count=count_inc, av_model=av_model)
--- src/jax/dog.py
+++ src/jax/dog.py
@@ -1,20 +1,47 @@
-from typing import Optional, NamedTuple
-
 import chex
 import jax
-import jax.numpy as jnp
 import jax.lax as lax
+import jax.numpy as jnp
+import operator
+import optax
 from optax import ScalarOrSchedule
-from optax._src import base, combine, transform
+from optax._src import base, combine
+from optax._src import utils
 from optax._src.alias import _scale_by_learning_rate
+from typing import Optional, NamedTuple
+
+
+# modified from https://github.com/deepmind/optax/blob/master/optax/_src/contrib/mechanic.py ###########
+@jax.jit
+def _tree_sum(tree_x):
+    """Compute sum(tree_x)."""
+    sums = jax.tree_util.tree_map(jnp.sum, tree_x)
+    return jax.tree_util.tree_reduce(operator.add, sums)
+
+
+@jax.jit
+def _tree_squared_norm(tree, tree2=None):
+    """Compute the l2 norm ||tree1 - tree2|| if tree2 is given,
+  otherwise compute ||tree1||."""
+    if tree2 is not None:
+        tree = jax.tree_util.tree_map(lambda x, y: x - y, tree, tree2)
+    return _tree_sum(jax.tree_map(lambda x: jnp.sum(x ** 2), tree))
+
+
+@jax.jit
+def _tree_norm(tree, tree2=None):
+    return jnp.sqrt(_tree_squared_norm(tree, tree2))
+
+
+#################################
 
 
 class ScaleByDogState(NamedTuple):
     """State for the DoG algorithm."""
-    step_count: chex.Array  # shape=(), dtype=jnp.int32.  # TODO - seems like this is not the way to define scalar?
+    step_count: chex.Array  # shape=(), dtype=jnp.int32.
     rbar: chex.Array
     g: chex.Array
-    init_buffer: chex.Array
+    init_buffer: base.Params
 
 
 def scale_by_dog(
@@ -23,16 +50,14 @@ def scale_by_dog(
         init_eta: Optional[float] = None,
         weight_decay: float = 0.0,
 ) -> base.GradientTransformation:
+    accumulator_dtype = utils.canonicalize_dtype(None)
 
     def init_fn(params):
         return ScaleByDogState(step_count=jnp.zeros([], jnp.int32),
                                rbar=jnp.zeros([], jnp.float32),
                                g=jnp.zeros([], jnp.float32),
-                               init_buffer=jnp.hstack([v.flatten() for p in params.values() for v in p.values()]))
-        # mu = jax.tree_util.tree_map(  # First moment
-        #     lambda t: jnp.zeros_like(t, dtype=mu_dtype), params)
-        # nu = jax.tree_util.tree_map(jnp.zeros_like, params)  # Second moment
-        # return ScaleByAdamState(count=jnp.zeros([], jnp.int32), mu=mu, nu=nu)
+                               init_buffer=jax.tree_util.tree_map(
+                                   lambda t: jnp.zeros_like(t, dtype=accumulator_dtype), params))
 
     def update_fn(updates, state, params=None):
         # updates are the gradients if they were not scaled yet
@@ -41,35 +66,30 @@ def scale_by_dog(
             # updates = jax.tree_multimap(lambda p, g: g + weight_decay * p, params, updates)
 
         def first_update(_):
-            # TODO - maybe should use tree_map here instead of for loops
-            init_buffer = jnp.hstack([v.flatten() for p in params.values() for v in p.values()])
-            params_norm = jnp.linalg.norm(
-                jnp.hstack([v.flatten() for p in params.values() for v in p.values()]))  # biases and kernels
+            init_buffer = jax.tree_util.tree_map(jnp.copy, params)
+            params_norm = _tree_norm(params)
             rbar = reps_rel * (1 + params_norm)
 
-            grads_flat = jnp.hstack([v.flatten() for g in updates.values() for v in g.values()])
-            g = state.g + jnp.sum(grads_flat ** 2)
-
+            g = state.g + _tree_squared_norm(updates)
             eta = jnp.array(init_eta if init_eta is not None else rbar / jnp.sqrt(g + eps))
 
             return rbar, init_buffer, g, eta
 
         def general_update(_):
             init_buffer = state.init_buffer
-            params_flat = jnp.hstack(jnp.hstack([v.flatten() for p in params.values() for v in p.values()]))
-            rbar = jnp.maximum(state.rbar, jnp.linalg.norm(params_flat - init_buffer))
+            # params_flat = jnp.hstack(jnp.hstack([v.flatten() for p in params.values() for v in p.values()]))
+            # rbar = jnp.maximum(state.rbar, jnp.linalg.norm(params_flat - init_buffer))
+            rbar = jnp.maximum(state.rbar, _tree_norm(params, tree2=init_buffer))
 
-            grads_flat = jnp.hstack([v.flatten() for g in updates.values() for v in g.values()])
-            g = state.g + jnp.sum(grads_flat ** 2)
-
+            g = state.g + _tree_squared_norm(updates)
             eta = rbar / jnp.sqrt(g + eps)
 
             return rbar, init_buffer, g, eta
 
         rbar, init_buffer, g, eta = lax.cond(state.step_count == 0, first_update, general_update, None)
 
-        step_count = state.step_count + 1
-        updates = jax.tree_util.tree_map(lambda u: eta*u, updates)  # scaling by lr will come later if needed
+        step_count = optax.safe_int32_increment(state.step_count)  # should be used in dog as well
+        updates = jax.tree_util.tree_map(lambda u: eta * u, updates)  # scaling by lr will come later if needed
 
         return updates, ScaleByDogState(step_count=step_count, rbar=rbar, g=g, init_buffer=init_buffer)
 
